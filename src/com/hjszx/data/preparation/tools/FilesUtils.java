5e503599dfede90927977d8458e10faf3ad7bf0c
hxy <houxiaoyu@bosi-edu.org>
2015-08-04 11:11:05 +0800

    updates

--- src/com/hjszx/data/preparation/tools/FilesUtils.java
+++ src/com/hjszx/data/preparation/tools/FilesUtils.java
@@ -1,27 +1,21 @@
 package com.hjszx.data.preparation.tools;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 
 public class FilesUtils {  
 	
     private static FilesUtils instance;  
-	
-    OutputStreamWriter writer = null;
-    FileOutputStream fos = null;
+	private String file;
+
     
     private FilesUtils (String file){
-    	
-    	try {
-			fos = new FileOutputStream(file);
-			writer = new OutputStreamWriter(fos);
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}	
+    	this.file = file;
     	
     }  
   
@@ -35,25 +29,46 @@ public class FilesUtils {
 	public void appendContents(String content) {
 		// TODO Auto-generated method stub
 		
+		FileWriter fw = null;
 		try {
-			
-			writer.append(content);
-
+			File f=new File(this.file);
+			fw = new FileWriter(f, true);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		PrintWriter pw = new PrintWriter(fw);
+		pw.println(content);
+		pw.flush();
+		
+		try {
+			fw.flush();
+			pw.close();
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+//		
+//		
+//	    OutputStreamWriter writer = null;
+//	    FileOutputStream fos = null;
+//	    
+//    	try {
+//			fos = new FileOutputStream(file);
+//			writer = new OutputStreamWriter(fos);
+//
+//			writer.append(content);
+//			writer.flush();
+//			writer.close();
+//			
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 			
 	}
 
 	public void close() {
 		// TODO Auto-generated method stub
-		try {
-			this.writer.flush();
-			this.writer.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}  
 }  
--- src/com/hjszx/data/preparation/tools/Main.java
+++ src/com/hjszx/data/preparation/tools/Main.java
@@ -7,22 +7,22 @@ public class Main {
 		String file = "C:\\professor.txt";
 		String url = CollectDataTools.URL_BASE;
 		StringBuilder sb = new StringBuilder();
-		
-		int i=1;
-		int j=1;
+		FilesUtils fu = FilesUtils.getInstance(file);
+		int i=16140;
+		int j=14342;
 		for(;i<=CollectDataTools.MAX_INT;i++){
 			
 			Professor p = CollectDataTools.resolve(CollectDataTools.get(url+i),i);
 			
 			if(!p.name.equals("")){
 				System.out.println(j+". "+ p);
-				sb.append(j+". "+ p + "\n");
+				fu.appendContents(j+". "+ p + "\n");
 				j++;
 			}
 		}
-		FilesUtils fu = FilesUtils.getInstance(file);
-		fu.appendContents(sb.toString());
+		
 		fu.close();
+
 	}
 
 }
